916519c9befeb496270d67361608b7131ff92b4f
oabidoye <oabidoye@yahoo.com>
2022-02-28 11:51:13 +0000

    adding application request model

---
+++ ApplicationProcessor/Models/ApplicationRequestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ULaw.ApplicationProcessor.Enums;
+
+namespace Ulaw.ApplicationProcessor.Models
+{
+    internal class ApplicationRequestModel
+    {
+        public Guid ApplicationId { get; private set; }
+        public string Faculty { get; private set; }
+        public string CourseCode { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public string Title { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public bool RequiresVisa { get; private set; }
+
+        public DegreeGradeEnum DegreeGrade { get; set; }
+        public DegreeSubjectEnum DegreeSubject { get; set; }
+    }
+}
